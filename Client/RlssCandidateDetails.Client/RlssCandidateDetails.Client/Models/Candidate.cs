4f4ad0c4d3874ae484af6f9408dd9722f62584e4
midnightgizmo <scottamillard@hotmail.com>
2022-08-04 15:02:10 +0100

    add Candidate details page

---
+++ Client/RlssCandidateDetails.Client/RlssCandidateDetails.Client/Models/Candidate.cs
@@ -0,0 +1,10 @@
+﻿namespace RlssCandidateDetails.Client.Models
+{
+    public class Candidate
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string SocietyNumber { get; set; }
+    }
+}
---
+++ Client/RlssCandidateDetails.Client/RlssCandidateDetails.Client/ViewModels/CandidatesVM.cs
@@ -0,0 +1,22 @@
+﻿using RlssCandidateDetails.Client.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace RlssCandidateDetails.Client.ViewModels
+{
+    public class CandidatesVM
+    {
+        [StringLength(10, ErrorMessage="Name is too long")]
+        public string FirstNameFilter {get;set;}
+        [StringLength(10, ErrorMessage = "Surname is too long")]
+        public string SurnameFilter {get;set;}
+        [RegularExpression(@"^\d{8}$", ErrorMessage ="Must be 8 digits long")]
+        public int? SocietyNumber { get; set; } = null;
+
+        public List<Candidate> CandidatesToShow { get; set; }
+
+        public CandidatesVM()
+        {
+            this.CandidatesToShow = new List<Candidate>();
+        }
+    }
+}
